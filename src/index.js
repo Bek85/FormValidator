583a49dace50a298877b0e2b022b632571f33f47
Bekhzod Akbarov <stx.webdev@gmail.com>
2020-10-11 03:06:32 +0500

    Some code refactoring by creating checkRequired and getFieldName functions

--- src/index.js
+++ src/index.js
@@ -7,22 +7,7 @@ const checkPasswordInput = document.getElementById('checkPassword');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  if (usernameInput.value === '')
-    showError(usernameInput, 'Username is required');
-  else showSuccess(username);
-
-  if (emailInput.value === '') showError(emailInput, 'Email is required');
-  else if (!isValidEmail(emailInput.value))
-    showError(email, 'Email is not valid');
-  else showSuccess(emailInput);
-
-  if (passwordInput.value === '')
-    showError(passwordInput, 'Password is required');
-  else showSuccess(passwordInput);
-
-  if (checkPasswordInput.value === '')
-    showError(checkPasswordInput, 'Enter your password again');
-  else showSuccess(checkPasswordInput);
+  checkRequired([usernameInput, emailInput, passwordInput, checkPasswordInput]);
 });
 
 const showError = (input, message) => {
@@ -39,4 +24,16 @@ const showSuccess = (input) => {
 const isValidEmail = (email) => {
   const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regEx.test(String(email).toLowerCase());
+};
+
+const checkRequired = (formInputs) => {
+  formInputs.forEach((input) => {
+    if (input.value.trim() === '')
+      showError(input, `${getFieldName(input)} is required`);
+    else showSuccess(input);
+  });
+};
+
+const getFieldName = (input) => {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 };
