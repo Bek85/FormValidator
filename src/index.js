ae96767ce4c256bee837c190db5b7c6b0749f519
Bekhzod Akbarov <stx.webdev@gmail.com>
2020-10-11 02:46:47 +0500

    Simple form validation without refactoring

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-import test from './modules/globals';
+const form = document.getElementById('form');
+const usernameInput = document.getElementById('username');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const checkPasswordInput = document.getElementById('checkPassword');
 
-test();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (usernameInput.value === '')
+    showError(usernameInput, 'Username is required');
+  else showSuccess(username);
+
+  if (emailInput.value === '') showError(emailInput, 'Email is required');
+  else if (!isValidEmail(emailInput.value))
+    showError(email, 'Email is not valid');
+  else showSuccess(emailInput);
+
+  if (passwordInput.value === '')
+    showError(passwordInput, 'Password is required');
+  else showSuccess(passwordInput);
+
+  if (checkPasswordInput.value === '')
+    showError(checkPasswordInput, 'Enter your password again');
+  else showSuccess(checkPasswordInput);
+});
+
+const showError = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.classList.add('error');
+  formControl.querySelector('small').textContent = message;
+};
+
+const showSuccess = (input) => {
+  const formControl = input.parentElement;
+  formControl.classList.add('success');
+};
+
+const isValidEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regEx.test(String(email).toLowerCase());
+};
